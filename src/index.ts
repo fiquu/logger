38dfdbbe731abae3a3fb34ea59ceb397064ba148
Santiago G. Marín <santiago.marin@dotstudio.io>
2020-02-25 16:01:20 -0300

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-export default true;
+import { createLogger as _createLogger, transports, format, Logger, LoggerOptions } from 'winston';
+
+const { timestamp, prettyPrint } = format;
+
+/**
+ * Creates a new logger with the provided component name and `@fiquu/logger` as module name.
+ *
+ * @param {string} component The name of the component to log for.
+ *
+ * @returns {Logger} The logger instance.
+ */
+export function createLogger(component: string): Logger {
+  const options: LoggerOptions = {
+    level: process.env.LOG_LEVEL || 'info',
+    transports: [new transports.Console()],
+    defaultMeta: {
+      module: '@fiquu/logger',
+      component
+    },
+    format: format.combine(
+      timestamp(),
+      prettyPrint()
+    )
+  };
+
+  return _createLogger(options);
+}
